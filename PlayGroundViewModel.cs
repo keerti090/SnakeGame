5735fd5692e7011b08f53b9d4c9689738a171122
keerti Hegde <kirti.vishwanath21@gmail.com>
2019-09-10 15:48:23 +0530

    Add files via upload

---
+++ PlayGroundViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace synchronizationContext.View_Models
+{
+    public class PlayGroundViewModel :ViewModelBase
+    {
+        ObservableCollection<SnakeBlockViewModel> _snake=new ObservableCollection<SnakeBlockViewModel>();
+
+        public PlayGroundViewModel()
+        {
+         Snake.Add(new SnakeBlockViewModel() {IsHead = true});
+         Snake.Add(new SnakeBlockViewModel() {IsHead = false});
+
+        }
+        public ObservableCollection<SnakeBlockViewModel> Snake
+        {
+            get { return _snake; }
+            set
+            {
+                _snake = value;
+                OnPropertyChanged(nameof(Snake));
+            }
+        }
+    }
+}
